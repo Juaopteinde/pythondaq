8c9f67ea0f2ea909d9e7ee39df4f49752d5fbfab
Juaopteinde <143963603+Juaopteinde@users.noreply.github.com>
2024-11-04 15:37:11 +0100

    added start of controller script
    added class with methods to:
    Check what device is connected
    Set output voltage
    Read output voltage in ADC or voltage

--- test.py
+++ test.py
@@ -1,16 +1,41 @@
-import csv
-import os
+import pyvisa
 
-current_directory = os.getcwd()
-entries = os.listdir(current_directory)
-print(entries)
+rm = pyvisa.ResourceManager("@py")
+print(rm.list_resources())
 
-filename = "metingen_0.csv"
-counter = 0
-while filename in entries:
-    filename = f"metingen_{counter}.csv"
-    counter += 1
 
-with open(f"{filename}", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["I", "U"])
+# Convert raw 10 bit ADC value to volt
+def raw_value_to_volt(raw_value):
+    step = 3.3 / 1023
+    volt = raw_value * step
+    return volt
+
+
+class ArduinoVisaDevice:
+
+    # Define the device used by the rest of the class
+    def __init__(self, port):
+        self.device = rm.open_resource(
+            f"{port}", read_termination="\r\n", write_termination="\n"
+        )
+
+    # Return identification string of the device connected to the given port
+    def get_identification(self):
+        identification = self.device.quert("*IDN?")
+        return identification
+
+    # Set ADC OUTPUT voltage on channel 0, ranging 0 - 1023
+    def set_output_value(self, value):
+        self.device.query(f"OUT:CH0 {value}")
+        print(f"Set ADC OUTPUT voltage on channel 0 to {value}")
+
+    # Read and return the voltage on the given channel, in ADC values (0 - 1023)
+    def get_input_value(self, channel):
+        input_value = self.device.query(f"MEAS:CH{channel}?")
+        return input_value
+
+    # Read and return the voltage on the given channel, in voltage (0 - 3.3 V)
+    def get_input_voltage(self, channel):
+        step = 3.3 / 1023
+        input_voltage = (int(self.device.query(f"MEAS:CH{channel}?"))) * step
+        return input_voltage
