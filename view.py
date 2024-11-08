2984197a95cee96764bdc108d0c5bba705ca5d39
Juaopteinde <juaopteinde@Outlook.com>
2024-11-08 15:44:13 +0100

    Added asking user for start/stop/repeat input

--- view.py
+++ view.py
@@ -5,10 +5,16 @@ from diode_experiment import DiodeExperiment
 # Define port connected to arduino
 port = "ASRL4::INSTR"
 
+start, stop = input(
+    "In what range do you want to scan? Give start and stop value seperated by a space. e.g. 800 900 \n"
+).split()
+
+repeats = input("How many times do you want to repeat the measurement? \n")
+
 # Run measurements and save the data
 measurement = DiodeExperiment(port)
 voltages_LED, currents_LED, errors_voltages_LED, errors_currents_LED = measurement.scan(
-    0, 1023, 3  # start stop repeats
+    int(start), int(stop), 3  # start stop repeats
 )
 
 # Plot the data
